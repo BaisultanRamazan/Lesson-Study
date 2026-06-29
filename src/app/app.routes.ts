95cc09d53055f3f16628dd6a3f7ebbcd5d25e68a
BaisultanRamazan <baisultan.ramazan@gmail.com>
2026-06-29 17:22:08 +0600

    fix: test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard'; // Проверь путь
 import { DocumentRedactorComponent } from './document-redactor/document-redactor'; // Импортируем наш файл
+import { Login } from './login/login';
 
 export const routes: Routes = [
+  // 1. При входе на сайт сразу отправляем на страницу логина/твоего меню
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // 2. Твои существующие маршруты
+  { path: 'login', component: Login },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'redactor/:id', component: DocumentRedactorComponent }, // Маршрут для открытия конкретного документа
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: 'redactor', component: DocumentRedactorComponent },
+
+  // 3. Если что-то пошло не так — тоже на логин
+  { path: '**', redirectTo: '/login' }
 ];
